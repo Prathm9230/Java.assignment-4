123680af96e8e1a6d3dceeabcb5464fd4036aff2
Prathm <jawahireprathmesh@gmail.com>
2023-06-20 19:56:53 +0530

    Calculate daily employee wage

--- src/com/bridgelabz/employeewage/Employeewage.java
+++ src/com/bridgelabz/employeewage/Employeewage.java
@@ -5,11 +5,17 @@ import java.sql.SQLOutput;
 public class Employeewage {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int emphrs=0;
         double empcheck = Math.floor(Math.random() * 10) % 2;
         if (empcheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present.");
-        } else {
-            System.out.println("Employee is Absent.");
+           emphrs=8;
+          }
+        else
+        {
+            emphrs=0;
         }
+        int empwage = EMP_RATE_PER_HOUR * emphrs;
+        System.out.println("Employee daily wage is:"+ empwage);
     }
 }
