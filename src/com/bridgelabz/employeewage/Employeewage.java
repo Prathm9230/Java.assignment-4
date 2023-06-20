6cb58b4e84b0570a9911e01de5db671b4868478b
Prathm <jawahireprathmesh@gmail.com>
2023-06-20 21:45:39 +0530

    Monthly employee wage

--- src/com/bridgelabz/employeewage/Employeewage.java
+++ src/com/bridgelabz/employeewage/Employeewage.java
@@ -3,24 +3,29 @@ package com.bridgelabz.employeewage;
 import java.sql.SQLOutput;
 
 public class Employeewage {
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+
     public static void main(String[] args) {
-        int IS_FULL_TIME = 2;
-        int IS_PART_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
-        int emphrs=0;
-        double empcheck = Math.floor(Math.random() * 10) % 3;//here we have added part time so,taken 3.
-        if (empcheck == IS_FULL_TIME) {
-               emphrs=16;
-          }
-        else if(empcheck==IS_PART_TIME)
-        {
-            emphrs=8;
+        int emphrs = 0, Totalempwage = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empcheck = (int)Math.floor(Math.random() * 10) % 3;
+            switch (empcheck) {
+                case IS_FULL_TIME:
+                    emphrs = 16;
+                    break;
+                case IS_PART_TIME:
+                    emphrs = 8;
+                    break;
+                default:
+                    emphrs = 0;
+            }
+            int empwage = EMP_RATE_PER_HOUR * emphrs;
+            Totalempwage = Totalempwage + empwage;
+            System.out.println("Employee daily wage is:" + empwage);
         }
-        else
-        {
-            emphrs=0;
-        }
-        int empwage = EMP_RATE_PER_HOUR * emphrs;
-        System.out.println("Employee daily wage is:"+ empwage);
+        System.out.println("Totalempwage is:" + Totalempwage);
     }
 }
